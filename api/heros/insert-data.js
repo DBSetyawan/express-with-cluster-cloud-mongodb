8d2a4f325b8bae33f59593fe83285eaf358f909f
Daniel <artexsdns@gmail.com>
2020-05-29 15:40:32 +0700

    Latest

--- api/heros/insert-data.js
+++ api/heros/insert-data.js
@@ -1,11 +1,9 @@
 const { MongoClient } = require("mongodb");
  
-// Replace the following with your Atlas connection string                                                                                                                                        
 const url = "mongodb+srv://daniel:asdasdasjdh7324%28k@cluster0-sxrjq.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
  
- // The database to use
  const dbName = "test";
                       
  async function run() {
@@ -14,23 +12,18 @@ const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser:
          console.log("Connected correctly to server");
          const db = client.db(dbName);
 
-         // Use the collection "people"
          const col = db.collection("people");
 
-         // Construct a document                                                                                                                                                              
          let personDocument = {
              "name": { "first": "Alan", "last": "Turing" },
-             "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
-             "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
+             "birth": new Date(1912, 5, 23), // juni 23, 1912                                                                                                                                 
+             "death": new Date(1954, 5, 7),  // jni 7, 1954                                                                                                                                  
              "contribs": [ "Turing machine", "Turing test", "Turingery" ],
              "views": 1250000
          }
 
-         // Insert a single document, wait for promise so we can read it back
          const p = await col.insertOne(personDocument);
-         // Find one document
          const myDoc = await col.findOne();
-         // Print to the console
          console.log(myDoc);
 
         } catch (err) {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://daniel:asdasdasjdh7324%28k@cluster0-sxrjq.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useUnifiedTopology: true });
+const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("sample_airbnb").collection("listingsAndReviews");
   // perform actions on the collection object
